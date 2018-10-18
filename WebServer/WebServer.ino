26624ee23cfad8102ed86433b6ed2d6db05891a6
Jardel Alves <jardelfilho2001@gmail.com>
2018-10-17 21:30:13 -0300

    Add files via upload
    Links de referência:
    https://www.youtube.com/watch?v=P-fJVk3saK0&t=53s
    https://randomnerdtutorials.com/esp32-web-server-arduino-ide/

---
+++ WebServer/WebServer.ino
@@ -0,0 +1,95 @@
+#include <WiFi.h>
+
+const char* ssid     = "";
+const char* password = "";
+
+const int output2 = 2;
+
+String header;
+String outputState = "OFF";
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(output2, OUTPUT);
+  digitalWrite(output2, LOW);
+
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Conectado a WiFi");
+  Serial.println("Endereço de IP: ");
+  Serial.print(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   
+
+  if (client) {                             
+    Serial.println("Novo cliente");          
+    String currentLine = "";                
+    while (client.connected()) {            
+      if (client.available()) {             
+        char c = client.read();             
+        Serial.write(c);                    
+        header += c;
+        if (c == '\n') {                    
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            if (header.indexOf("GET /2/on") >= 0) {
+              Serial.println("GPIO 2 Ligado");
+              outputState = "ON";
+              digitalWrite(output2, HIGH);
+            } else if (header.indexOf("GET /2/off") >= 0) {
+              Serial.println("GPIO 2 Desligado");
+              outputState = "OFF";
+              digitalWrite(output2, LOW);
+            }
+            
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            client.println("<p>GPIO 2 " + outputState + "</p>");      
+            if (outputState=="OFF") {
+              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+            
+            client.println("</body></html>");
+            
+            client.println();
+            break;
+          } else { 
+            currentLine = "";
+          }
+        } else if (c != '\r') {  
+          currentLine += c;      
+        }
+      }
+    }
+    header = "";
+    client.stop();
+    Serial.println("Cliente desconectado");
+    Serial.println("");
+  }
+}
